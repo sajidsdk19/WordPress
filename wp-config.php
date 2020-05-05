027ca7cd28a0bcbab7029fe74a331a4a9794e5f3
Sajid Khan <sajid_19khan@yahoo.com>
2020-05-06 01:06:01 +0500

    Update and rename wp-config-sample.php to wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -18,24 +18,21 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
-
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'aqigxihbyrqsux' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '6cb24a14f759fba63cc52e064f1f481323655573915b7ba0cb246a34c06bdf03' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'ec2-54-86-170-8.compute-1.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
 
 /**#@+
  * Authentication Unique Keys and Salts.
